d9115ed10a5e9ca41bf8a2c56a71d3a34d999b17
Derek van der Mark <derek.vandermark@gmail.com>
2023-10-29 21:28:17 -0400

    Set up basic navigation layout

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-
+	
 	if (req.url === '/') {
 		console.log("here1")
 		const renderedHtml = render('views/layout.pug');
